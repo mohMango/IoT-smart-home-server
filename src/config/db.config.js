5a35e83ea075f9611eaa4916f51da8834907e3d7
Mohamed Ibraheem <moh.mango12@gmail.com>
2021-07-04 23:23:04 +0200

    push to github

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,12 +1,6 @@
 export default {
-  HOST: "us-cdbr-east-03.cleardb.com",
-  USER: "b666c4d232d84b",
-  PASSWORD: "db19ec0c",
-  DATABASE: "heroku_e1b7c0bba6cd0af",
+  HOST: process.env.HOST,
+  USER: process.env.USER,
+  PASSWORD: process.env.PASSWORD,
+  DATABASE: process.env.DATABASE,
 };
-// export default {
-//   HOST: "freedb.tech",
-//   USER: "freedbtech_moebra",
-//   PASSWORD: "moha010m",
-//   DATABASE: "freedbtech_iot",
-// };
