91d1a06d595b9b0ef84b09823b20cc724d03eed0
Mohamed Ibraheem <moh.mango12@gmail.com>
2021-07-04 11:07:07 +0200

    fix bug

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -74,7 +74,7 @@ User.findBy = (type, value, result) => {
 //   });
 // };
 
-User.updateById = (user, result) => {
+User.updateById = (id, user, result) => {
   sql.getConnection((err, connection) => {
     if (err) throw err;
 
@@ -93,7 +93,7 @@ User.updateById = (user, result) => {
         }
 
         console.log("updated user: ", { id: id, ...user });
-        result(null, { user });
+        result(null, { id: id, ...user });
       }
     );
     connection.release();
